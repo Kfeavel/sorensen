0ff4082ded037393b2201cdf6da137897be54563
Keeton Feavel <keeton@xyr.is>
2022-03-26 14:24:50 -0400

    Finish compression draft

--- SorensenCompression/main.cpp
+++ SorensenCompression/main.cpp
@@ -12,13 +12,21 @@
 #include <unistd.h>
 
 
+struct CompressionData
+{
+    uint64_t bitCountTotal;     // Total number of set bits
+    uint64_t byteCountTotal;    // Total number of bytes
+};
+
+
 int main(int argc, char* const argv[])
 {
     std::fstream fileIn;
+    std::fstream fileOut;
     
     int8_t opt = 0;
-    bool doCompress = true;
-    while ((opt = getopt(argc, argv, "cuf:")) != -1) {
+    bool doCompress = false;
+    while ((opt = getopt(argc, argv, "cui:o:")) != -1) {
         switch (opt) {
             case 'c':
                 doCompress = true;
@@ -26,55 +34,55 @@ int main(int argc, char* const argv[])
             case 'u':
                 doCompress = false;
                 break;
-            case '?':
+            case 'i':
+                fileIn.open(optarg, std::fstream::in | std::fstream::binary);
+                if (!fileIn.is_open() || !fileIn.good()) {
+                    std::cout << "Failed to open input file" << std::endl;
+                    return EXIT_FAILURE;
+                }
+                break;
+            case 'o':
+                fileOut.open(optarg, std::fstream::out | std::fstream::binary);
+                if (!fileOut.is_open() || !fileOut.good()) {
+                    std::cout << "Failed to open output file" << std::endl;
+                    return EXIT_FAILURE;
+                }
+                break;
+            case 'h':
             default:
                 std::cout << "TODO: Usage" << std::endl;
                 return EXIT_SUCCESS;
          }
     }
     
-    std::cout << "File path: " << std::endl;
-    std::string path;
-    std::getline(std::cin, path);
-    if (path.empty()) {
-        return EXIT_FAILURE;
-    }
-    
-    std::cout << "Opening '" << path << "'" << std::endl;
-    fileIn.open(path, std::fstream::in | std::fstream::out | std::fstream::binary);
-    if (!fileIn.is_open()) {
-        std::cout << "Failed to open file" << std::endl;
-        return EXIT_FAILURE;
-    }
-    
-    if (!fileIn.good()) {
-        std::cout << "File is not good" << std::endl;
-        return EXIT_FAILURE;
-    }
-    
     if (doCompress) {
+        CompressionData data = {
+            .bitCountTotal = 0,
+            .byteCountTotal = 0,
+        };
+        
         char byte;
-        uint16_t bitCountTotal = 0;
         while (fileIn.read(&byte, sizeof(byte)))
         {
-            uint16_t bitCountCurr = 0;
-            std::cout << std::bitset<CHAR_BIT>(byte).to_string() << "\t";
-            
+            data.byteCountTotal++;
             while (byte) {
                 if (byte % 2 == 1) {
-                    bitCountCurr++;
-                    bitCountTotal++;
+                    data.bitCountTotal++;
                 }
                 
                 byte = byte >> 1;
             }
-            
-            std::cout << "Current: " << bitCountCurr << " Total: " << bitCountTotal << std::endl;
         }
+        
+        std::cout << "Writing results..." << std::endl;
+        fileOut.write(reinterpret_cast<const char*>(&data), sizeof(data));
     } else {
         std::cout << "TODO: Decompression" << std::endl;
     }
 
+    std::cout << "Done!" << std::endl;
     fileIn.close();
+    fileOut.close();
+    
     return EXIT_SUCCESS;
 }
