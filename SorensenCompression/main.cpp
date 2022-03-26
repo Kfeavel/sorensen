c92caef5a04a0e7d7c609fcaa6206dd78fd31b72
Keeton Feavel <keeton@xyr.is>
2022-03-26 12:42:48 -0400

    Initial Commit

---
+++ SorensenCompression/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SorensenCompression
+//
+//  Created by Keeton Feavel on 3/26/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
