4a5a6d2f87a5f410ac310753a63ee6007fe2b80b
Keeton Feavel <keeton@xyr.is>
2022-03-26 15:34:16 -0400

    Record hash of file

--- SorensenCompression/main.cpp
+++ SorensenCompression/main.cpp
@@ -5,6 +5,7 @@
 //  Created by Keeton Feavel on 3/26/22.
 //
 
+#include <vector>
 #include <bitset>
 #include <string>
 #include <fstream>
@@ -17,6 +18,7 @@ struct CompressionData
 {
     uint64_t bitCountTotal;     // Total number of set bits
     uint64_t byteCountTotal;    // Total number of bytes
+    uint64_t hash;              // Hash of bitset
 };
 
 std::streamsize GetFileSizeFromStream(std::fstream& file)
@@ -64,25 +66,38 @@ int main(int argc, char* const argv[])
          }
     }
     
+    std::vector<bool> bitset;
     if (doCompress) {
         Stopwatch([&]() {
             CompressionData data = {
                 .bitCountTotal = 0,
                 .byteCountTotal = 0,
+                .hash = 0,
             };
             
-            uint8_t byte;
+            uint8_t byte = 0;
             while (fileIn.read(reinterpret_cast<char*>(&byte), sizeof(byte)))
             {
                 data.byteCountTotal++;
-                while (byte) {
-                    if (byte % 2 == 1) {
+                for (uint8_t i = 0; i < CHAR_BIT; i++)
+                {
+                    bitset.push_back(((byte >> i) & 1) != 0);
+                    if (((byte >> i) & 1) != 0) {
                         data.bitCountTotal++;
                     }
-                    
-                    byte = byte >> 1;
                 }
             }
+            
+            data.hash = std::hash<std::vector<bool>>{}(bitset);
+            
+#if defined(DEBUG)
+            std::cout << "Hash: " << std::hex << std::uppercase << data.hash << std::endl;
+            std::cout << "0b";
+            for (bool b : bitset) {
+                std::cout << (b ? "1" : "0");
+            }
+            std::cout << std::endl;
+#endif
             
             fileOut.write(reinterpret_cast<const char*>(&data), sizeof(data));
         });
@@ -102,7 +117,7 @@ int main(int argc, char* const argv[])
             // FIXME: How should I error check this?
             fileIn.read(reinterpret_cast<char*>(&data), sizeof(data));
             
-            std::cout << "TODO: Implement decompression" << std::endl;
+            bitset.reserve(data.byteCountTotal * CHAR_BIT);
         });
     }
 
