b48881fb5f8f8b2b141c4537cddca09c66d874de
Keeton Feavel <keeton@xyr.is>
2022-03-27 22:52:54 -0400

    Code cleanup, docs, and user confirmation

--- SorensenCompression/main.cpp
+++ SorensenCompression/main.cpp
@@ -5,6 +5,7 @@
 //  Created by Keeton Feavel on 3/26/22.
 //
 
+#include <cmath>
 #include <vector>
 #include <bitset>
 #include <string>
@@ -17,7 +18,7 @@
 
 struct CompressionData
 {
-    uint64_t bitCountTotal;     // Total number of set bits
+    uint64_t setBitsCountTotal; // Total number of set / high bits
     uint64_t byteCountTotal;    // Total number of bytes
     uint64_t hash;              // Hash of bitset
 };
@@ -31,24 +32,23 @@ static std::streamsize GetFileSizeFromStream(std::fstream& file)
     return length;
 }
 
-static void binary_write(std::fstream& fout, std::vector<bool>* x)
+static void WriteVectorBool(std::fstream& fileOut, std::vector<bool>* x)
 {
-    std::vector<bool>::size_type i = 0;
-    std::vector<bool>::size_type n = x->size();
-    while (i < n) {
-        uint8_t aggr = 0;
-        for (uint8_t mask = 1; mask > 0 && i < n; ++i, mask <<= 1) {
+    for (size_t i = 0; i < x->size(); i++) {
+        uint8_t byte = 0;
+        for (uint8_t mask = 1; mask > 0; mask <<= 1) {
             if (x->at(i)) {
-                aggr |= mask;
+                byte |= mask;
             }
         }
         
-        fout.write(reinterpret_cast<const char*>(&aggr), sizeof(uint8_t));
+        fileOut.write(reinterpret_cast<const char*>(&byte), sizeof(uint8_t));
     }
 }
 
 int main(int argc, char* const argv[])
 {
+    int ret = EXIT_SUCCESS;
     std::fstream fileIn;
     std::fstream fileOut;
     
@@ -84,14 +84,15 @@ int main(int argc, char* const argv[])
     }
     
     std::vector<bool>* bitset = new std::vector<bool>;
+    CompressionData data = {
+        .setBitsCountTotal = 0,
+        .byteCountTotal = 0,
+        .hash = 0,
+    };
+    
     if (doCompress) {
+        std::cout << "Compressing..." << std::endl;
         Stopwatch([&]() {
-            CompressionData data = {
-                .bitCountTotal = 0,
-                .byteCountTotal = 0,
-                .hash = 0,
-            };
-            
             uint8_t byte = 0;
             while (fileIn.read(reinterpret_cast<char*>(&byte), sizeof(byte)))
             {
@@ -100,55 +101,77 @@ int main(int argc, char* const argv[])
                 {
                     bitset->push_back(((byte >> i) & 1) != 0);
                     if (((byte >> i) & 1) != 0) {
-                        data.bitCountTotal++;
+                        data.setBitsCountTotal++;
                     }
                 }
             }
             
             data.hash = std::hash<std::vector<bool>>{}(*bitset);
             fileOut.write(reinterpret_cast<const char*>(&data), sizeof(data));
+            std::cout << "Done!" << std::endl;
         });
     } else {
+        // Get data about the resulting file before performing the decompression so that we can
+        // properly warn the end user that decompression may eat most of their RAM, and/or take
+        // a literal eternity to decompress. For this reason chickening out is the default answer.
+        
+        if (GetFileSizeFromStream(fileIn) != sizeof(data))
+        {
+            std::cout << "Compression data is corrupt";
+            ret = EXIT_FAILURE;
+            goto fail;
+        }
+        
+        fileIn.read(reinterpret_cast<char*>(&data), sizeof(data));
+        if (!fileIn.good()) {
+            std::cout << "Failed to read compression data";
+            ret = EXIT_FAILURE;
+            goto fail;
+        }
+        
+        // TODO: Not quite accurate yet. Need time for 6 bytes to get better equation.
+        double approximateSeconds = (45.576 * std::pow(data.byteCountTotal, 2)) - (209.28 * data.byteCountTotal) + 190.95;
+        
+        std::cout << "Decompression requires " << data.byteCountTotal << " bytes of memory." << std::endl;
+        std::cout << "Approximate time to decompress: " << approximateSeconds << " seconds" << std::endl;
+        std::cout << "Continue? [y/N] ";
+        if (std::tolower(std::getchar()) != 'y') {
+            ret = EXIT_FAILURE;
+            goto fail;
+        }
+        
+        std::cout << "Decompressing..." << std::endl;
         Stopwatch([&]() {
-            CompressionData data = {
-                .bitCountTotal = 0,
-                .byteCountTotal = 0,
-                .hash = 0,
-            };
+            // For `std::next_permutation` to work we need to fill in all of the zeros up front
+            // and then fill in the remaining ones at the end. From there, `std::next_permutation`
+            // will walk all possible iterations, effectively moving the ones from the last portion
+            // of the bitset to the front.
             
-            if (GetFileSizeFromStream(fileIn) != sizeof(data))
-            {
-                std::cout << "Compression data is corrupt";
-                exit(EXIT_FAILURE);
-            }
-            
-            // FIXME: How should I error check this?
-            fileIn.read(reinterpret_cast<char*>(&data), sizeof(data));
-            
-            for (size_t i = 0; i < (data.byteCountTotal * CHAR_BIT) - data.bitCountTotal; i++) {
+            for (size_t i = 0; i < (data.byteCountTotal * CHAR_BIT) - data.setBitsCountTotal; i++) {
                 bitset->push_back(0);
             }
             
-            for (size_t i = 0; i < data.bitCountTotal; i++) {
+            for (size_t i = 0; i < data.setBitsCountTotal; i++) {
                 bitset->push_back(1);
             }
             
-            std::cout << "Decompressing... (This may take a while)" << std::endl;
+            // Perform the actual decompression
             do {
                 size_t hash = std::hash<std::vector<bool>>{}(*bitset);
                 if (hash == data.hash)
                 {
-                    std::cout << "Got it!" << std::endl;
-                    binary_write(fileOut, bitset);
+                    std::cout << "Done!" << std::endl;
+                    WriteVectorBool(fileOut, bitset);
                     break;
                 }
             } while(std::next_permutation(bitset->begin(), bitset->end()));
         });
     }
 
+fail:
     fileIn.close();
     fileOut.close();
     delete bitset;
     
-    return EXIT_SUCCESS;
+    return ret;
 }
